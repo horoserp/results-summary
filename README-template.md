d7bdaab7d307d89337a62c57f432f5fac6a0908f
Robert Horosewski <horoserp@yahoo.com>
2024-11-20 22:04:14 -0500

    Fix: Change JS elements so that they group things for use with flex. Add JS elements to create corner borders. Style corner borders. Add classes to corner divs to further style. Styled corner borders.

--- README-template.md
+++ README-template.md
@@ -71,6 +71,7 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - JS Create elements and add properties
 - JS Add elements programatically
 - CSS color-mix
+- CSS create borders only on the corners of a div
 
 Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
 
--- script.js
+++ script.js
@@ -10,18 +10,71 @@ xmlhttp.onreadystatechange = function () {
       newImage = document.createElement("img");
       newDiv = document.createElement("section");
       newSpan = document.createElement("span");
+      newSpanTwo = document.createElement("span");
+      newSpanBold = document.createElement("strong");
+      newCorner2 = document.createElement("div");
+      newCorner1 = document.createElement("div");
+      newCorner3 = document.createElement("div");
+      newCorner4 = document.createElement("div");
 
-      // Add newImage now for display purposes
-      newDiv.appendChild(newImage);
+      // Add newImage to newDiv
+      newSpanTwo.appendChild(newImage);
+
+      // Add classes to elements
+      newDiv.classList.add("subset");
+      newSpanTwo.classList.add("span_flex");
+      newCorner1.classList.add("corner");
+      newCorner2.classList.add("corner");
+      newCorner3.classList.add("corner");
+      newCorner4.classList.add("corner");
+
+      switch (key) {
+        case "0":
+          newCorner1.classList.add("red");
+          newCorner2.classList.add("red");
+          newCorner3.classList.add("red");
+          newCorner4.classList.add("red");
+          break;
+        case "1":
+          newCorner1.classList.add("yellow");
+          newCorner2.classList.add("yellow");
+          newCorner3.classList.add("yellow");
+          newCorner4.classList.add("yellow");
+          break;
+        case "2":
+          newCorner1.classList.add("green");
+          newCorner2.classList.add("green");
+          newCorner3.classList.add("green");
+          newCorner4.classList.add("green");
+          break;
+        case "3":
+          newCorner1.classList.add("blue");
+          newCorner2.classList.add("blue");
+          newCorner3.classList.add("blue");
+          newCorner4.classList.add("blue");
+          break;
+      }
 
       // Populate elements
       newImage.src = myObj[key].icon;
-      newDiv.classList.add("subset");
-      newDiv.innerHTML += myObj[key].category;
-      newSpan.innerHTML = myObj[key].score + " / 100";
+      newSpanTwo.innerHTML += myObj[key].category;
+      newSpanBold.innerHTML = myObj[key].score;
 
-      // Add elements to DOM
+      // Add newSpanBold to newSpan
+      newSpan.appendChild(newSpanBold);
+
+      // Populate newSpan
+      newSpan.innerHTML += " / 100";
+
+      // Add newSpan and newSpanTwo to newDiv
+      newDiv.appendChild(newSpanTwo);
       newDiv.appendChild(newSpan);
+      newDiv.appendChild(newCorner1);
+      newDiv.appendChild(newCorner2);
+      newDiv.appendChild(newCorner3);
+      newDiv.appendChild(newCorner4);
+
+      // Add element to DOM
       subset[0].parentNode.insertBefore(
         newDiv,
         continueButton[0].previousSibling
